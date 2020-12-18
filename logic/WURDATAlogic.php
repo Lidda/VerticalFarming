bfe5bb44b119de81604cc3efff353e38e3117bd1
Lidda <amblink@hotmail.com>
2020-12-18 12:07:06 +0100

    Save data from all WUR search results

--- logic/WURDATAlogic.php
+++ logic/WURDATAlogic.php
@@ -13,49 +13,62 @@
 
     function processXML(){
       $sources = [];
-      foreach($this->xml->bron as $s){
-        $title = "";
-        $author = "";
-        $year = "";
-        $link = "";
-        $abstractEN = "";
-        $abstractNL = "";
-        $keytermsEN = "";
-        $keytermsNL = "";
+      $pageLeft = true;
 
-        if (isset($s->titel)){
-          $title = ((string)$s->titel);
-        }
-        if (isset($s->auteur)){
-          $author = ((string)$s->auteur);
-        }
-        if (isset($s->jaar)){
-          $year = ((string)$s->jaar);
-        }
-        if (isset($s->link['href'])){
-          $link = ((string)$s->link['href']);
+      //loop to go through multiple pages of search results
+      while($pageLeft){
+        foreach($this->xml->bron as $s){
+          $title = "";
+          $author = "";
+          $year = "";
+          $link = "";
+          $abstractEN = "";
+          $abstractNL = "";
+          $keytermsEN = "";
+          $keytermsNL = "";
+
+          if (isset($s->titel)){
+            $title = ((string)$s->titel);
+          }
+          if (isset($s->auteur)){
+            $author = ((string)$s->auteur);
+          }
+          if (isset($s->jaar)){
+            $year = ((string)$s->jaar);
+          }
+          if (isset($s->link['href'])){
+            $link = ((string)$s->link['href']);
+          }
+
+          if (isset($s->abstract->engels)){
+            $abstractEN = ((string)$s->abstract->engels);
+          }
+          if (isset($s->abstract->nederlands)){
+            $abstractNL = ((string)$s->abstract->nederlands);
+          }
+
+          if (isset($s->trefwoorden->engels)){
+            $keytermsEN= ((string)$s->trefwoorden->engels);
+          }
+          if (isset($s->trefwoorden->nederlands)){
+            $keytermsNL= ((string)$s->trefwoorden->nederlands);
+          }
+
+          $keywords = $keytermsEN."- ".$keytermsNL;
+          $source = new Source($link, $title, $author, $year, $abstractNL, $abstractEN, $keywords);
+          $sources[] = $source;
         }
 
-        if (isset($s->abstract->engels)){
-          $abstractEN = ((string)$s->abstract->engels);
+        //check if there is a next page
+        if(!isset($this->xml->next['href'])){
+          $pageLeft = false;
+        } else {
+          //set xml to that of the next page
+          $nextPageLink = (string)($this->xml->next['href']);
+          $this->xml = $this->wurcurl->getXML($nextPageLink);
         }
-        if (isset($s->abstract->nederlands)){
-          $abstractNL = ((string)$s->abstract->nederlands);
-        }
-
-        if (isset($s->trefwoorden->engels)){
-          $keytermsEN= ((string)$s->trefwoorden->engels);
-        }
-        if (isset($s->trefwoorden->nederlands)){
-          $keytermsNL= ((string)$s->trefwoorden->nederlands);
-        }
-
-        $keywords = $keytermsEN."- ".$keytermsNL;
-        $source = new Source($link, $title, $author, $year, $abstractNL, $abstractEN, $keywords);
-        $sources[] = $source;
       }
 
-      //print_r($sources);
       return $sources;
     }
   }
